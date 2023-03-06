24289a111f42aa18d757469705da770a4d7a7a6d
Abhijeet Prasad <aprasad@sentry.io>
2023-03-06 12:20:27 +0100

    add monkey patching

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,8 +3,39 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import * as client from "react-dom/client";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+const oldClientHydrateRoot = client.hydrateRoot;
+
+client.hydrateRoot = new Proxy(oldClientHydrateRoot, {
+  apply: (wrappingTarget, thisArg, args) => {
+    const oldOnRecoverableError = args[2].onRecoverableError;
+
+    args[2].onRecoverableError = new Proxy(oldOnRecoverableError, {
+      apply: (inner_wrappingTarget, inner_thisArg, inner_args) => {
+        const error = inner_args[0];
+        const errorInfo = inner_args[1];
+
+        console.log(error);
+        console.log(errorInfo);
+        console.log(errorInfo.componentStack);
+
+        const errorBoundaryError = new Error(error.message);
+        errorBoundaryError.name = `React ErrorBoundary ${errorBoundaryError.name}`;
+        errorBoundaryError.stack = errorInfo.componentStack;
+
+        // Using the `LinkedErrors` integration to link the errors together.
+        error.cause = errorBoundaryError;
+
+        Sentry.captureException(error, { contexts: { react: errorInfo } });
+      },
+    });
+
+    return wrappingTarget.apply(thisArg, args);
+  },
+});
 
 Sentry.init({
   dsn: SENTRY_DSN,
